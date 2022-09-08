abff9cb31dc3feb140f41aa2658a6d10a84e7218
Skyler Ferreira <sky_3489@icloud.com>
2022-09-08 14:47:32 -0700

    all basic pages for functionality created

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,50 @@
 const router = require('express').Router();
 
 
+router.get('/', async (req, res) => {
 
+    try {
+        res.render('home');
+    } catch (err) {
+        res.status(500).send(err)
+    }
+});
 
+router.get("/dashboard", async (req, res) => {
+    try {
+        res.render("dashboard");
+    } catch (err) {
+        res.status(500).send(err)
+    }
 
+});
 
+router.get("/login", async (req, res) => {
+    try {
+        res.render("login");
+    } catch (err) {
+        res.status(500).send(err)
+    }
 
+});
 
+router.get("/sighnup", async (req, res) => {
+    try {
+        res.render("sighnup");
+    } catch (err) {
+        res.status(500).send(err)
+    }
 
+});
 
+router.get("/newpost", async (req, res) => {
+    try {
+        res.render("newPost");
+    } catch (err) {
+        res.status(500).send(err)
+    }
+
+});
 
 
 module.exports = router;
